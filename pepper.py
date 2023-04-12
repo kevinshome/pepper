767b002ea09e78f308e958b20ba6b80291c7ab64
Noah Tanner <50159154+kevinshome@users.noreply.github.com>
2023-04-12 15:34:56 -0400

    move things around

--- pepper.py
+++ pepper.py
@@ -5,17 +5,11 @@ from http.client import HTTPResponse
 from html.parser import HTMLParser
 
 __version__ = "0.0.0"
-RAW_PEP_URL_BASE = "https://peps.python.org/pep-"
+RAW_PEP_URL_BASE = "https://raw.githubusercontent.com/python/peps/main/pep-"
+PEP_URL_BASE = "https://peps.python.org/pep-"
 PEP_0_URL = "https://peps.python.org/pep-0000"
 
-def fatal_error(message: str):
-    sys.stderr.write('pepper: ' + message + '\n')
-    raise SystemExit(1)
-
-def parse_pepfile(data: bytes):
-    parsed_data = {"header": {}}
-
-    class PepFileParser(HTMLParser):
+class PepFileHeaderParser(HTMLParser):
         def __init__(self) -> None:
             super().__init__()
             self._last_tag = None
@@ -24,6 +18,7 @@ def parse_pepfile(data: bytes):
             self._last_key = None
             self._list_head = False
             self._title_read = False
+            self.parsed_data = {}
         def handle_starttag(self, tag, attrs) -> None:
             self._last_tag = self._current_tag
             self._current_tag = tag
@@ -34,39 +29,51 @@ def parse_pepfile(data: bytes):
                     if attr == "class" and value == "page-title":
                         self._title_read = True
                         pep, title = data.split(" – ")
-                        parsed_data["raw_title"] = data
-                        parsed_data["title"] = title
-                        parsed_data["number"] = pep.split()[1]
+                        self.parsed_data["raw_title"] = data
+                        self.parsed_data["title"] = title
+                        self.parsed_data["number"] = pep.split()[1]
+                        return
             if self._current_tag == "dt":
                 self._current_tag = None
-                parsed_data["header"][data] = ""
+                self.parsed_data[data] = ""
                 self._last_key = data
             if self._current_tag == "dd":
                 self._current_tag = None
-                parsed_data["header"][self._last_key] = data
+                self.parsed_data[self._last_key] = data
             if self._current_tag == "abbr":
                 self._current_tag = None
-                parsed_data["header"][self._last_key] = data
+                self.parsed_data[self._last_key] = data
             if self._current_tag == "a" and self._last_tag == "dd":
                 self._list_head = True
                 self._current_tag = None
-                parsed_data["header"][self._last_key] = []
+                self.parsed_data[self._last_key] = []
             if self._list_head:
                 if data == ",\n" or data == "\n":
                     return
-                parsed_data["header"][self._last_key].append(data)
+                self.parsed_data[self._last_key].append(data)
         def handle_endtag(self, tag) -> None:
             if tag == "dd":
                 self._list_head = False
 
-    parser = PepFileParser()
-    parser.feed(data.decode(errors='xmlcharrefreplace'))
+def fatal_error(message: str):
+    sys.stderr.write('pepper: ' + message + '\n')
+    raise SystemExit(1)
 
-    parsed_data["header"]["Author"] = parsed_data["header"]["Author"].split(', ')
-    parsed_data["header"].pop("Discussions-To", None)
-    parsed_data["header"].pop("Resolution", None)
+def parse_pepfile_header(data: bytes):
+    full_parsed_data = {}
+    decoded_data = data.decode(errors='xmlcharrefreplace')
 
-    return parsed_data
+    # parse PEP header information
+    head_parser = PepFileHeaderParser()
+    head_parser.feed(decoded_data)
+    head_parser.parsed_data["Author"] = head_parser.parsed_data["Author"].split(', ')
+    head_parser.parsed_data.pop("Discussions-To", None)
+    head_parser.parsed_data.pop("Resolution", None)
+    full_parsed_data["raw_title"] = head_parser.parsed_data.pop("raw_title")
+    full_parsed_data["title"] = head_parser.parsed_data.pop("title")
+    full_parsed_data["number"] = head_parser.parsed_data.pop("number")
+    full_parsed_data["header"] = head_parser.parsed_data
+    return full_parsed_data
 
 class Commands:
     def help(_):
@@ -84,13 +91,13 @@ class Commands:
 
     def info(_, pep_id: str):
         try:
-            res: HTTPResponse = urlopen(RAW_PEP_URL_BASE + pep_id.zfill(4))
+            res: HTTPResponse = urlopen(PEP_URL_BASE + pep_id.zfill(4))
         except HTTPError as exc:
             if exc.status == 404:
                 fatal_error(f"PEP {pep_id} not found...")
             fatal_error(f"Recieved error status code '{exc.status}' from python.org")
 
-        parsed_pep = parse_pepfile(res.read())
+        parsed_pep = parse_pepfile_header(res.read())
         print(parsed_pep["raw_title"], end='\n\n')
         for item in parsed_pep["header"].items():
             print('\t', end='')

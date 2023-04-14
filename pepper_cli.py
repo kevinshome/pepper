b9737a560876a6da65cf086aefa5da519e2b78a6
Noah Tanner <50159154+kevinshome@users.noreply.github.com>
2023-04-13 20:47:37 -0400

    black format

--- pepper_cli.py
+++ pepper_cli.py
@@ -20,9 +20,18 @@ except ModuleNotFoundError:
     WEBVIEW = None
 
 PEP_TYPES = {
-    "Informational": ("I", "Non-normative PEP containing background, guidelines or other information relevant to the Python ecosystem"),
-    "Process": ("P", "Normative PEP describing or proposing a change to a Python community process, workflow or governance"),
-    "Standards Track": ("S", "Normative PEP with a new feature for Python, implementation change for CPython or interoperability standard for the ecosystem")
+    "Informational": (
+        "I",
+        "Non-normative PEP containing background, guidelines or other information relevant to the Python ecosystem",
+    ),
+    "Process": (
+        "P",
+        "Normative PEP describing or proposing a change to a Python community process, workflow or governance",
+    ),
+    "Standards Track": (
+        "S",
+        "Normative PEP with a new feature for Python, implementation change for CPython or interoperability standard for the ecosystem",
+    ),
 }
 PEP_STATUSES = {
     "Accepted": ("A", "Normative proposal accepted for implementation"),
@@ -33,17 +42,19 @@ PEP_STATUSES = {
     "Rejected": ("R", "Formally declined and will not be accepted"),
     "Superseded": ("S", "Replaced by another succeeding PEP"),
     "Withdrawn": ("W", "Removed from consideration by sponsor or authors"),
-    "Draft": ("<No Letter>", "Proposal under active discussion and revision")
+    "Draft": ("<No Letter>", "Proposal under active discussion and revision"),
 }
+
 
 class KeyTextWrapper(TextWrapper):
     def __init__(self, offset_size: int = 0, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self.width = shutil.get_terminal_size().columns - offset_size
-        self.subsequent_indent = (' '*offset_size)
+        self.subsequent_indent = " " * offset_size
         self.break_long_words = False
         self.break_on_hyphens = False
         self.max_lines = 4
+
 
 class PepZeroParser(HTMLParser):
     def __init__(self) -> None:
@@ -60,6 +71,7 @@ class PepZeroParser(HTMLParser):
         self._last_tag = self._current_tag
         self._current_tag = tag
         self._current_attrs = attrs
+
     def handle_data(self, data) -> None:
         if self._current_tag == "section":
             for attr, value in self._current_attrs:
@@ -72,36 +84,39 @@ class PepZeroParser(HTMLParser):
         if self._last_tag == "td" and self._current_tag == "abbr":
             self._current_tag = None
             for attr, value in self._current_attrs:
-                if attr == "title" and value.split(', ')[0] in PEP_TYPES:
-                    _type, _status = value.split(', ')
+                if attr == "title" and value.split(", ")[0] in PEP_TYPES:
+                    _type, _status = value.split(", ")
                     self._current_pep["type"] = _type
                     self._current_pep["status"] = _status
                     self._current_pep_col += 2
                     return
         if self._last_tag == "td" and self._current_tag == "a":
             self._current_tag = None
-            if self._current_pep_col == 2: # number
+            if self._current_pep_col == 2:  # number
                 self._current_pep["number"] = int(data)
                 self._current_pep_col += 1
-            else: # title
+            else:  # title
                 self._current_pep["title"] = data
                 self._current_pep_col += 1
         if self._current_tag == "td" and self._current_pep_col == 4:
             self._current_tag = None
             self._current_pep["authors"] = []
-            for author in data.split(','):
+            for author in data.split(","):
                 self._current_pep["authors"].append(author.strip())
             self._current_pep_col = 0
             self.parsed_data.append(self._current_pep)
             self._current_pep = {}
+
     def handle_endtag(self, tag) -> None:
         if tag == "section" and self._read_head:
             self._read_head = False
+
     @classmethod
     def parse(cls, data: bytes) -> dict:
         parser = cls()
-        parser.feed(data.decode(errors='xmlcharrefreplace'))
+        parser.feed(data.decode(errors="xmlcharrefreplace"))
         return parser.parsed_data
+
 
 class PepFileHeaderParser(HTMLParser):
     def __init__(self) -> None:
@@ -113,10 +128,12 @@ class PepFileHeaderParser(HTMLParser):
         self._list_head = False
         self._title_read = False
         self.parsed_data = {}
+
     def handle_starttag(self, tag, attrs) -> None:
         self._last_tag = self._current_tag
         self._current_tag = tag
         self._current_attrs = attrs
+
     def handle_data(self, data) -> None:
         if self._current_tag == "h1" and not self._title_read:
             for attr, value in self._current_attrs:
@@ -140,12 +157,12 @@ class PepFileHeaderParser(HTMLParser):
         if self._current_tag == "a" and self._last_tag == "dd":
             self._current_tag = None
             if data == "Discourse thread":
-                for attr,value in self._current_attrs:
+                for attr, value in self._current_attrs:
                     if attr == "href":
                         self.parsed_data[self._last_key] = value
                         return
             if data == "Discourse message":
-                for attr,value in self._current_attrs:
+                for attr, value in self._current_attrs:
                     if attr == "href":
                         self.parsed_data[self._last_key] = value
                         return
@@ -155,27 +172,33 @@ class PepFileHeaderParser(HTMLParser):
             if data == ",\n" or data == "\n":
                 return
             self.parsed_data[self._last_key].append(data)
+
     def handle_endtag(self, tag) -> None:
         if tag == "dd":
             self._list_head = False
+
     @classmethod
     def parse(cls, data: bytes) -> dict:
         full_parsed_data = {}
-        decoded_data = data.decode(errors='xmlcharrefreplace')
+        decoded_data = data.decode(errors="xmlcharrefreplace")
 
         # parse PEP header information
         head_parser = cls()
         head_parser.feed(decoded_data)
-        head_parser.parsed_data["Author"] = head_parser.parsed_data["Author"].split(', ')
+        head_parser.parsed_data["Author"] = head_parser.parsed_data["Author"].split(
+            ", "
+        )
         full_parsed_data["raw_title"] = head_parser.parsed_data.pop("raw_title")
         full_parsed_data["title"] = head_parser.parsed_data.pop("title")
         full_parsed_data["number"] = head_parser.parsed_data.pop("number")
         full_parsed_data["header"] = head_parser.parsed_data
         return full_parsed_data
 
+
 def fatal_error(message: str) -> None:
-    sys.stderr.write('pepper: ' + message + '\n')
+    sys.stderr.write("pepper: " + message + "\n")
     raise SystemExit(1)
+
 
 def format_searched_pep(pep_obj: dict) -> str:
     _string = ""
@@ -191,14 +214,11 @@ def format_searched_pep(pep_obj: dict) -> str:
 
     return _string
 
+
 def _view_helper(pep_id, url):
-    WEBVIEW.create_window(
-        f'PEP {pep_id}', 
-        url, 
-        height=800, 
-        frameless=True
-    )
+    WEBVIEW.create_window(f"PEP {pep_id}", url, height=800, frameless=True)
     WEBVIEW.start()
+
 
 class Commands:
     def help(_):
@@ -227,19 +247,27 @@ class Commands:
         except HTTPError as exc:
             if exc.status == 404:
                 fatal_error(f"PEP {pep_id} not found...")
-            fatal_error(f"Recieved error status code '{exc.status}' from peps.python.org")
+            fatal_error(
+                f"Recieved error status code '{exc.status}' from peps.python.org"
+            )
 
         return url
 
     def view(self, pep_id: str):
         pep_url = self._get_pep_url(pep_id)
         if WEBVIEW is None:
-            fatal_error("the 'view' command requires the `webview` extra to be installed.")
+            fatal_error(
+                "the 'view' command requires the `webview` extra to be installed."
+            )
         print(f"Pulling up PEP {pep_id} in a new window...")
-        proc = multiprocessing.Process(target=_view_helper, args=(pep_id, pep_url), daemon=False)
+        proc = multiprocessing.Process(
+            target=_view_helper, args=(pep_id, pep_url), daemon=False
+        )
         proc.start()
         print(f"PEP {pep_id} loaded ({proc.pid}), Bye!")
-        os._exit(0) # we call os._exit here to ensure the webview stays alive as an orphan, instead of dying along with the parent
+        os._exit(
+            0
+        )  # we call os._exit here to ensure the webview stays alive as an orphan, instead of dying along with the parent
 
     def open(self, pep_id: str):
         pep_url = self._get_pep_url(pep_id)
@@ -249,18 +277,18 @@ class Commands:
         return 0
 
     def keys(_):
-        sys.stdout.write('\n')
+        sys.stdout.write("\n")
         print("PEP Types Key")
         print("------------------")
         for title, info in PEP_TYPES.items():
-            pad_length = 7+len(title)
+            pad_length = 7 + len(title)
             print(f"{title} ({info[0]}) - {KeyTextWrapper(pad_length).fill(info[1])}")
         print("PEP Status Key")
         print("------------------")
         for title, info in PEP_STATUSES.items():
-            pad_length = 6+len(title)+len(info[0])
+            pad_length = 6 + len(title) + len(info[0])
             print(f"{title} ({info[0]}) - {KeyTextWrapper(pad_length).fill(info[1])}")
-        sys.stdout.write('\n')
+        sys.stdout.write("\n")
         return 0
 
     def info(_, pep_id: str):
@@ -269,15 +297,17 @@ class Commands:
         except HTTPError as exc:
             if exc.status == 404:
                 fatal_error(f"PEP {pep_id} not found...")
-            fatal_error(f"Recieved error status code '{exc.status}' from peps.python.org")
+            fatal_error(
+                f"Recieved error status code '{exc.status}' from peps.python.org"
+            )
 
         parsed_pep = PepFileHeaderParser.parse(res.read())
         print(parsed_pep["raw_title"])
-        print(f"({PEP_URL_BASE + pep_id.zfill(4)})", end='\n\n')
+        print(f"({PEP_URL_BASE + pep_id.zfill(4)})", end="\n\n")
         for item in parsed_pep["header"].items():
-            print('\t', end='')
+            print("\t", end="")
             if not isinstance(item[1], list):
-                print(': '.join(item))
+                print(": ".join(item))
             elif item[0] == "Author":
                 print(f"{item[0]}: {item[1][0]}")
                 item[1].pop(0)
@@ -288,11 +318,10 @@ class Commands:
                 item[1].pop(0)
                 for entry in item[1]:
                     s += f" {entry},"
-                print(s.strip(','))
+                print(s.strip(","))
         return 0
-    
+
     def search(_, attribute, *query_list):
-        
         try:
             res: HTTPResponse = urlopen(PEP_0_URL)
         except HTTPError as exc:
@@ -316,7 +345,9 @@ class Commands:
                     if query.lower() in str(pep[attribute]).lower():
                         peps.append(format_searched_pep(pep))
             if not peps:
-                sys.stderr.write(f"No PEP found matching the following query: '{query}'\n")
+                sys.stderr.write(
+                    f"No PEP found matching the following query: '{query}'\n"
+                )
                 return 1
 
             print("---------------------------------------")
@@ -325,7 +356,7 @@ class Commands:
             for pep in peps:
                 print(pep)
 
-        sys.stdout.write('\n')
+        sys.stdout.write("\n")
         return 0
 
     def run_cmd(self, cmd, args):
@@ -336,11 +367,14 @@ class Commands:
                 func = ref
         if func is None or func.__name__ == "run_cmd":
             fatal_error(f"No such command ({cmd})...")
-        
+
         param_count = len(inspect.signature(func).parameters)
         if param_count > len(args):
-            fatal_error(f"Not enough arguments (expected {param_count}, got {len(args)}).")
+            fatal_error(
+                f"Not enough arguments (expected {param_count}, got {len(args)})."
+            )
         raise SystemExit(func(*args))
+
 
 def main():
     if len(sys.argv) == 1:
